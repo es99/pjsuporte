e3fdd32ff63de62959f0223030dbcc494f72a786
Engels Souza <engels.franca@icloud.com>
2023-06-22 23:03:30 -0300

    criado banco para solicitacoes de cadastro e o mesmo foi inserido no flask-admin

--- infopublic_usuarios/extensions/admin/__init__.py
+++ infopublic_usuarios/extensions/admin/__init__.py
@@ -1,9 +1,10 @@
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from infopublic_usuarios.extensions.db import db, User
+from infopublic_usuarios.extensions.db import db, User, Cadastros
 
 admin = Admin()
 admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Cadastros, db.session))
 
 
 def init_app(app):
